3431b9a9db4ad0629ba41daaba42161ab0727628
unknown <darknemiroff@gmail.com>
2023-01-14 22:03:06 +0400

    mini-shop-design

---
+++ app.js
@@ -0,0 +1,18 @@
+let cart = document.querySelector('.cart');
+let cartfield = document.querySelector('.cart-field');
+let add = document.querySelector('.add');
+let dd = document.querySelector('.dd');
+
+
+cart.addEventListener('click', () =>{
+    document.querySelector('.dd').style.display = "flex";
+    document.querySelector('.dd').style.top = "-600px";
+})
+
+
+
+
+
+
+
+
